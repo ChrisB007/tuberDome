b5b141882d479a504c239c407c46424de81f2f4f
ChrisB007 <chrisbitoy@gmail.com>
2021-06-18 18:43:12 -0400

    updated sponsors page

--- src/Components/pages/sponsors.js
+++ src/Components/pages/sponsors.js
@@ -14,7 +14,7 @@ const features = [
     name: 'Budgeting',
     icon: CurrencyDollarIcon,
     description:
-      'Search and browse through channels in categories that aligns with your brand, view their rates per subscriber and/or begin negotiations by proposing your budget.',
+      'Search and browse through channels in categories that aligns with your brand, view their rates per subscriber and/or begin negotiations by proposing your own budget.',
   },
   {
     name: 'Launch',
